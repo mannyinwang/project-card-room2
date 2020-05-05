96ce9ababa7dd2a843779854146ca822c0381bdf
Manny Inwang <mannyinwang@gmail.com>
2020-05-04 22:58:59 -0700

    minor changes

--- config.py
+++ config.py
@@ -1,28 +1,28 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import func, or_, and_ # for NOW() timestamp function
+from sqlalchemy.sql import func, or_, and_  # for NOW() timestamp function
 from flask_migrate import Migrate
 import re  # the regex module    
 from flask_bcrypt import Bcrypt
-from flask_socketio import SocketIO 
+from flask_socketio import SocketIO
+
 app = Flask(__name__)
 
 # configurations to tell our app about the database we'll be connecting to
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///card-room2.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 # an instance of the ORM
 db = SQLAlchemy(app)
+
 # a tool for allowing migrations/creation of tables
 migrate = Migrate(app, db)
 
-EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 PWD_REGEX = re.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!*#?&])[A-Za-z\d@$!#*?&]{6,20}$")
 app.secret_key = "bbbafdskiezxfjopterwggggfdsafdsafsdawiofxxgf"
+
 # we are creating an object called bcrypt,
 # which is made by invoking the function Bcrypt with our app as an argume
-bcrypt = Bcrypt(app)  
+bcrypt = Bcrypt(app)
 socketio = SocketIO(app)
-
-
-
-print('welcome')
